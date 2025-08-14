e108aca1a5abd18e4fbfe52493fedf51622e9c59
Parbat Sunuwar <parbatwar@gmail.com>
2025-08-14 12:07:33 +0545

    Updated More links in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,6 +10,7 @@ export const Footer = () => {
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
+        <li><a href="https://www.linktr.ee/ParbatSunuwar" target="_blank" rel="noopener noreferrer">More+</a></li>
       </ul>
       <p className={styles.footerInfo}>Copyright &#169; 2025 Parbat Sunuwar. All Rights Reserved.</p>
     </footer>
